4ae724164edeb7ea50cec34c792b6c472bde6874
Vladimir <v.kumov@gmail.com>
2024-04-08 17:02:56 +0200

    footer, header, some new styled components

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { FooterContainer } from './styles'
+import {
+  NavGroupLink,
+  NavGroupLogo,
+  NavItemText,
+  NavLinks,
+  NavLogoText,
+} from '../Header/styles'
 
 function Footer() {
   return (
-    <div>
-    </div>
+    <FooterContainer>
+      <NavLinks>
+        <NavGroupLink to="/">
+          <NavItemText>About Us</NavItemText>
+        </NavGroupLink>
+
+        <NavGroupLink to="/">
+          <NavItemText>Press</NavItemText>
+        </NavGroupLink>
+
+        <NavGroupLink to="/">
+          <NavItemText>Blog</NavItemText>
+        </NavGroupLink>
+
+        <NavGroupLink to="/">
+          <NavItemText>iOS</NavItemText>
+        </NavGroupLink>
+
+        <NavGroupLink to="/">
+          <NavItemText>Android</NavItemText>
+        </NavGroupLink>
+      </NavLinks>
+    </FooterContainer>
   )
 }
 
