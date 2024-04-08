830f236d7a692a34eab08de8d49c236efa632dbd
Vladimir <v.kumov@gmail.com>
2024-04-08 16:18:53 +0200

    some changes in structure, routes added

---
+++ frontend/src/routes/AuthPages/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SectionContainer } from '../../../styles'
+
+function Login() {
+  return (
+    <SectionContainer>
+      <h1>Login Page</h1>
+    </SectionContainer>
+  )
+}
+
+export default Login
