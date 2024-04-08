06ba88b92bdcc9453553fffb1e9a3b75d3d5bed9
Your Name <you@example.com>
2024-04-08 15:44:43 +0200

    comment,review,restaurant models mainly done

---
+++ backend/comment/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from review.models import Review
+
+User = get_user_model()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.DO_NOTHING)
+    text_content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+
---
+++ backend/restaurant/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+def restaurant_directory_path(instance, filename):
+    return f'{instance.id}/restaurant/{filename}'
+
+
+CATEGORY_CHOICES = {
+    "Thai": "Thai Food",
+    "Asia": "Asia Food",
+    "Indian": "Indian Food",
+    "French": "French Food",
+    "Mexican": "Mexican Food",
+    "Italian": "Italian Food",
+}
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='restaurants')
+    country = models.CharField(max_length=150, blank=False)
+    street = models.CharField(max_length=150, blank=False)
+    city = models.CharField(max_length=100, blank=False)
+    zip = models.CharField(max_length=10)
+    website = models.URLField
+    phone = models.CharField(max_length=15, blank=False, null=True)
+    email = models.EmailField(max_length=100, unique=True)
+    opening_hours = models.IntegerField(max_length=15, blank=False, null=True)
+    price_level = models.IntegerField(max_length=15, blank=False, null=True)
+    image = models.ImageField(upload_to='restaurant_directory_path')
+
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, blank=False)
+
+
+def __str__(self):
+    return self.name
