9ec7d0184ce7dbf72cf6a27fbe3dfd3ad4622c69
Vladimir <v.kumov@gmail.com>
2024-04-10 14:12:30 +0200

    added nav menu in search, also new routes for search

---
+++ frontend/src/routes/Search/CatMenu/styles.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { SectionContainer } from '../../../styles'
+
+export const CatMenuContainer = styled(SectionContainer)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
