ce5cc7a40605e337f14e172c446f4b2f1aeabcc4
Vladimir <v.kumov@gmail.com>
2024-04-11 11:08:02 +0200

    changes in reviews

--- frontend/src/routes/Search/Reviews/ReviewCard/index.jsx
+++ frontend/src/routes/Search/Reviews/ReviewCard/index.jsx
@@ -1,11 +1,18 @@
 import { BaseArticle, DivWithLine } from '../../../../styles'
-import { ReviewCardInfo } from './styles'
+import { ReadMoreLink, ReviewCardInfo } from './styles'
 
 import ReviewReactionsComponent from '../ReviewReactions'
 import UserCardHeader from '../../Users/UserCard/UserCardHeader'
+import truncateText from '../../../../utils/useTruncate'
 import ReviewComments from '../ReviewComments'
 
+
 function ReviewCard({ review, user }) {
+  const maxLength = 50
+  const readMoreLink = `/search/restaurant/${review.restaurant.id}`
+  const truncatedText = truncateText(review.text_content, maxLength)
+  const isTextTruncated = review.text_content.length > maxLength
+
   return (
     <DivWithLine>
       <BaseArticle>
@@ -17,6 +24,14 @@ function ReviewCard({ review, user }) {
         />
         <ReviewCardInfo>
           <h3>{review.restaurant.name}</h3>
+          <p>
+            {truncatedText}
+            {isTextTruncated && (
+              <ReadMoreLink to={readMoreLink}>
+                Read more <i className="las la-long-arrow-alt-right"></i>
+              </ReadMoreLink>
+            )}
+          </p>
           <ReviewReactionsComponent countLikes={review.count_likes} />
 
           <h4>Last comments</h4>
