a1ef26de721a7c58c381a53620b58420664a2f40
Vytas <vytas.urbelis@gmail.com>
2024-04-11 09:16:15 +0200

    Added get specific review comments

--- backend/comment/views.py
+++ backend/comment/views.py
@@ -46,5 +46,6 @@ class DeleteCommentView(DestroyAPIView):
 class SpecificReviewCommentView(ListAPIView):
     serializer_class = CommentSerializer
     permission_classes = []
+
     def get_queryset(self):
         return Comment.objects.filter(review_id=self.kwargs['pk'])
