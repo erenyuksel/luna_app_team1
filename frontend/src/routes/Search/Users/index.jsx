4484d9d21cd5219c0a17a68bf0afc4089eed5e73
Vladimir <v.kumov@gmail.com>
2024-04-12 03:43:57 +0200

    restaurant page with some data and reviews

--- frontend/src/routes/Search/Users/index.jsx
+++ frontend/src/routes/Search/Users/index.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchUsers } from '../../../store/slices/usersSlice'
 import { GridContainer } from '../../../styles'
 import UserCard from './UserCard'
+import Loading from '../../../components/SmallElements/Loading'
 
 function UsersList() {
   const dispatch = useDispatch()
@@ -14,7 +15,7 @@ function UsersList() {
     dispatch(fetchUsers())
   }, [dispatch])
 
-  if (status === 'loading') return <div>Loading...</div>
+  if (status === 'loading') return <Loading />
   if (error) return <div>Error: {error}</div>
 
   return (
